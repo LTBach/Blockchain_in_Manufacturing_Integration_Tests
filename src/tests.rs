25bfab23d570ed624e99e59f5685b58abafa56f3
LTBach <21521845@gm.uit.edu.vn>
2022-09-22 19:10:19 +0700

    Update Code

--- src/tests.rs
+++ src/tests.rs
@@ -1,17 +1,19 @@
 use anyhow::Ok;
-use near_sdk::{json_types::U128, AccountId};
+
+#[allow(unused_imports)]
+use near_sdk::{json_types::U128, AccountId, Balance};
 use workspaces::{Contract, prelude::DevAccountDeployer, Worker, network::Sandbox, Account};
 use near_units::parse_near;
+#[allow(unused_imports)]
 use serde_json::{json, Value};
 use near_sdk::serde::{Deserialize, Serialize};
-use near_sdk::{Balance, Timestamp};
 
 pub type CommandId = String;
 pub type NameProduct = String;
 
 const CONTRACT_PATH: &str = "./wasm/contract.wasm";
 
-#[derive( Serialize, Deserialize, Clone)]
+#[derive( Serialize, Deserialize, Clone, Debug)]
 #[serde(crate = "near_sdk::serde")]
 
 pub struct Quality {
@@ -25,30 +27,22 @@ pub struct CommandDetail {
     pub command_id: CommandId,
     pub name_product: NameProduct,
     pub is_sell: bool,
-    pub amount_product: U128,
-    pub price_per_product: U128,
+    pub amount_product: u128,
+    pub price_per_product: Balance,
     pub quality: Option<Quality>,
     pub command_owner_id: AccountId,
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-
-    // Tao worker -> start worker
     let worker = workspaces::sandbox().await?;
-
-    // Dev-Deploy ecommerce payment contract len worker
     let contract_wasm = std::fs::read(CONTRACT_PATH)?;
     let contract: Contract = worker.dev_deploy(&contract_wasm).await?;
 
-    // Create account
-    // mainnet -> root account = near ex abc.near, xyz.near
-    // testnet -> root account = testnet, ex: vbidev.testnet
     let owner = worker.root_account().unwrap();
 
-    // tao account alice, vbidev.testnet, uit-payment-contract.vbidev.testnet
     let commander = owner.create_subaccount(&worker, "commander")
-                                            .initial_balance(parse_near!("30 N"))
+                                            .initial_balance(parse_near!("1000 N"))
                                             .transact()
                                             .await?
                                             .into_result()?;
@@ -68,7 +62,7 @@ async fn main() -> anyhow::Result<()> {
 }
 
 async fn test_add_buy_command(user: &Account, contract: &Contract, worker: &Worker<Sandbox>) -> anyhow::Result<()> {
-    let deposit = parse_near!("21 N");
+    let deposit = parse_near!("70 N");
 
     user.
         call(&worker, contract.id(), "add_command")
@@ -76,8 +70,8 @@ async fn test_add_buy_command(user: &Account, contract: &Contract, worker: &Work
             "command_id": "command_1",
             "name_product": "Iphone_14",
             "is_sell": false,
-            "amount_product": U128(1),
-            "price_per_product": U128(20),
+            "amount_product": 2,
+            "price_per_product": parse_near!("30 N"),
             "quality": null,
         }))?
         .deposit(deposit)
@@ -86,16 +80,18 @@ async fn test_add_buy_command(user: &Account, contract: &Contract, worker: &Work
 
     println!("      Passed ✅  add_buy_command");
 
-    let res_command: CommandDetail = user.call(worker, contract.id(), "get_command")
+    let res_command: CommandDetail = user.call(&worker, contract.id(), "get_command")
                                         .args_json(json!({
-                                            "command_id": "conmmand_1"
+                                            "command_id": "command_1"
                                         }))?
                                         .transact()
                                         .await?
                                         .json()?;
 
-    assert_eq!(res_command.name_product.to_string(), "Iphone_14");
-    assert_eq!(res_command.is_sell, false);
+    assert_eq!(res_command.name_product.to_string(), "Iphone_14", "WRONG_NAME_PRODUCT");
+    assert_eq!(res_command.is_sell, false, "WRONG_IS_SELL");
+    assert_eq!(res_command.amount_product, 2, "WRONG_AMOUNT_PRODUCT");
+    assert_eq!(res_command.price_per_product, parse_near!("30 N"), "WRONG_PRICE_PER_PRODUCT");
 
     println!("      Passed ✅  get_command");
 
